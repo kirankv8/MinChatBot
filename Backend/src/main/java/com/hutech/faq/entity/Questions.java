2da82a0353954554218e9a072b1e3ef4c4390741
kirankv8 <kirankv@hutechsolutions.com>
2023-05-19 14:46:28 +0530

    commit first

---
+++ Backend/src/main/java/com/hutech/faq/entity/Questions.java
@@ -0,0 +1,26 @@
+package com.hutech.faq.entity;
+
+import java.io.PrintWriter;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
+public class Questions {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String question;
+	private String answer;
+}
---
+++ Backend/src/test/java/com/hutech/faq/MiniFaQsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hutech.faq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniFaQsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
